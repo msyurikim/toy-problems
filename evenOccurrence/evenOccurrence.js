ccec95d2518bcee699eb84486f97952f4fe9198b
Yuri Kim <yurikim123@gmail.com>
2020-04-01 06:25:28 -1000

    completed toy problem: even occurrence

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -1,6 +1,6 @@
 /*
  * Find the first item that occurs an even number of times in an array.
- * Remember to handle multiple even-occurrence items and return the first one. 
+ * Remember to handle multiple even-occurrence items and return the first one.
  * Return null if there are no even-occurrence items.
 */
 
@@ -12,4 +12,26 @@
 
 var evenOccurrence = function(arr) {
   // Your code here.
+  var evenTimes = {};
+  //traverse original array
+  for (var i = 0; i < arr.length; i++) {
+    var numString = '"'+ arr[i] + '"';
+    if (evenTimes[numString] === undefined) {
+      evenTimes[numString] = 1;
+    } else {
+      evenTimes[numString] += 1;
+    }
+  }
+  //traverse object
+  console.log(evenTimes);
+
+  for (var i = 0; i < Object.keys(evenTimes).length; i++) {
+    var key = Object.keys(evenTimes)[i];
+    var num = key.slice(1, key.length-1);
+    if (evenTimes[key] % 2 === 0) return num;
+  }
+  return null;
 };
+
+var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
+console.log(onlyEven); //  4
