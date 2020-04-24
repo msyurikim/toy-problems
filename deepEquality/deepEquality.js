e15301cc3762fac46347689d17cc157e08c25847
Yuri Kim <yurikim123@gmail.com>
2020-04-24 06:50:57 -1000

    accounted for objects with same properties in different order

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,14 +12,33 @@
   *
   */
 var deepEquals = function(apple, orange) {
-  // var isEqual = false;
-
-  // var deepEqualsHelper
-  // return isEqual;
-  var appleStr = JSON.stringify(apple);
-  var orangeStr = JSON.stringify(orange);
-  return appleStr === orangeStr;
+  var deepEqualsHelper = (obj1, obj2) => {
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+      return false;
+    }
+    for (var key in obj1) {
+      if (!obj2.hasOwnProperty(key)) {
+        return false;
+      }
+      if (typeof obj1[key] === 'object' && obj1[key] !== null) {
+        return deepEqualsHelper(obj1[key], obj2[key]);
+      } else if (JSON.stringify(obj1[key]) !== JSON.stringify(obj2[key])) {
+        console.log(JSON.stringify(obj1[key]));
+        console.log(JSON.stringify(obj2[key]));
+        return false;
+      }
+    }
+    return true;
+  };
+  return deepEqualsHelper(apple, orange);
+  // return true;
+  // var appleStr = JSON.stringify(apple);
+  // var orangeStr = JSON.stringify(orange);
+  // return appleStr === orangeStr;
 };
 
 console.log(deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}})); // true
 console.log(deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}})); // false
+var a = { b: { c: { d: { e: 'potato' } } }, foo: 1 };
+var b = { foo: 1, b: { c: { d: { e: 'potato' } } } };
+console.log(deepEquals(a, b));  //true
