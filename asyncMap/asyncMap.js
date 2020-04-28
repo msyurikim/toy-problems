b31d04dc920bfc667c6da2cdfa127dd042f25e29
Yuri Kim <yurikim123@gmail.com>
2020-04-28 07:00:28 -1000

    completed asyncMap

--- asyncMap/asyncMap.js
+++ asyncMap/asyncMap.js
@@ -39,4 +39,40 @@
 
 
 var asyncMap = function(tasks, callback) {
+  var results = [];
+
+  // var prom1 = new Promise( () => tasks[0](cb) );
+  // var prom2 = new Promise( () => tasks[1](cb) );
+
+  var prom1 = new Promise( () => tasks[0]( str => { results[0] = str; }) );
+  var prom2 = prom1.then(new Promise( () => tasks[1]( str => { results[1] = str; }) ));
+
+  //when these are done
+  async function doTasks() {
+    await prom2.catch(e => {console.log(e)});
+    callback(results);
+  }
+  doTasks();
+
+  return results;
 };
+
+var results = asyncMap([
+  function(cb){
+    setTimeout(function(){
+    cb('one');
+  }, 200);
+},
+ function(cb){
+   setTimeout(function(){
+      cb('two');
+    }, 100);
+  }
+  ],
+ function(results){
+   // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+      console.log(results); // ['one', 'two']
+ });
+
+setTimeout(()=> console.log('results', results), 1000);
